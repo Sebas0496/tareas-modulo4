365bbf69773d13c4be388d8d443477ab06e042c1
Sebastian aristizabal <metrocaldas.1053850031@gmail.com>
2024-05-29 02:17:55 -0500

    envio tarea de la clase numero 2

---
+++ clase-2/projecto-clase-2/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() { 
+   
+  return (
+    <>
+     <div className='container'>  
+        <div className='description'>
+          <p>La película comienza con la pareja Charlie y Nicole Barber, quienes deciden asistir a terapia de pareja después de varios años de matrimonio. El proceso de terapia en pareja es una herramienta efectiva para ayudar a las parejas a resolver conflictos, mejorar la comunicación y fortalecer la relación.</p>
+          <p style={{color:'red'}}>!!GANADORA AL OSCAR¡¡</p>
+        </div>
+     </div>
+    </>
+  )
+}
+
+export default App
